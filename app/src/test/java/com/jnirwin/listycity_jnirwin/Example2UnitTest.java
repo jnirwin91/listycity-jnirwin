979752fbcfcd8e00bd3cb19951298770154a3f5c
Joshua Irwin <jnirwin91@gmail.com>
2020-11-26 19:19:22 -0700

    Fixed

---
+++ app/src/test/java/com/jnirwin/listycity_jnirwin/Example2UnitTest.java
@@ -0,0 +1,22 @@
+package com.jnirwin.listycity_jnirwin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class Example2UnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void addition_isCorrect2() {
+        assertEquals(3, 2 + 2);
+    }
+}
